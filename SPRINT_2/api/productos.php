e516a0cff239f073d80687f6bc5eb6c45c49bb21
Carlos Garrido <129065902+CarlosGarrido98@users.noreply.github.com>
2026-05-04 19:02:58 +0200

    feat: api

---
+++ SPRINT_2/api/productos.php
@@ -0,0 +1,14 @@
+<?php
+
+$conn = new mysqli("localhost", "root", "", "papel_verde");
+
+$sql = "SELECT * FROM productos";
+$result = $conn->query($sql);
+
+$productos = [];
+
+while ($row = $result->fetch_assoc()) {
+    $productos[] = $row;
+}
+
+echo json_encode($productos);
