c01a3cc287468e8925f51e546505edd5bafd099d
Carlos Garrido <129065902+CarlosGarrido98@users.noreply.github.com>
2026-06-07 17:57:03 +0200

    updates

--- MVC/views/galeria.php
+++ MVC/views/galeria.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Papel Verde</title>
+    <title>Nuestros Productos</title>
     <!-- Links a Bootstrap y css -->
     <link rel="icon"type="image/png" href="img/imgPapelVerde/Logoico.ico">
     <!-- Links a Bootstrap y css -->
