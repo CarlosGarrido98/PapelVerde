c8ef0d128747218f4670666b79dd5bb1376c0b0f
Carlos Garrido <129065902+CarlosGarrido98@users.noreply.github.com>
2026-06-14 16:47:48 +0200

    seo

--- MVC/views/home.php
+++ MVC/views/home.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Papel Verde</title>
+    <meta name="description" content="Papel Verde - Tienda online de libros, mangas y cómics. Descubre las mejores colecciones y novedades al mejor precio.">
     <link rel="icon" type="image/png" href="img/imgPapelVerde/Logoico.ico">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
