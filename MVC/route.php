2585f095f0dd156f9a5fcc4d360da1bba7128468
Carlos Garrido <129065902+CarlosGarrido98@users.noreply.github.com>
2026-06-15 16:15:28 +0200

    Comentarios route

--- MVC/route.php
+++ MVC/route.php
@@ -28,87 +28,88 @@ if (empty($request)) {
     $request = '/';
 }
 
-
+// Enrutamiento según el método HTTP
 switch ($method) {
+
+// Rutas que responden a peticiones GET
     case 'GET':
         switch ($request) {
 
+            // Reedirigir al Home
             case '/':
                 require_once 'controllers/HomeController.php';
                 break;
 
+            // Ir al Home    
             case '/home':
                 require_once 'controllers/HomeController.php';
                 break;
-
+            // Ir a la Galeria 
             case '/galeria':
                 require_once 'controllers/GaleriaController.php';
                 break;
-
+            // Acceder a Cada Producto
             case '/producto':
                 require_once 'controllers/ProductoController.php';
                 ProductoController::mostrarProducto();
                 break;   
-
+            // Acceder al Formulario 
             case '/formulario':
                 require_once 'views/formulario.php';
                 break;
-
+            // Acceder al Login
             case '/login':
                 require_once 'views/login.php';
                 break;
-
+            // Acceder al About 
             case '/about':
                 require_once 'views/about.php';
                 break;
-
+            // Acceder al Perfil
             case '/perfil':
                 require_once 'views/perfil.php';
                 break;
 
             //Vistas de Los Productos 
-
             case '/libros':
                 require_once 'controllers/LibrosController.php';
                 LibrosController::mostrar();
                 break;
-
             case '/mangas':
                 require_once 'controllers/MangasController.php';
                 MangasController::mostrar();
                 break;
-
             case '/comics':
                 require_once 'controllers/ComicsController.php';
                 ComicsController::mostrar();
                 break;
-                
-
+            
+            // Accedemos al buscador
             case '/buscar':
                 require_once 'controllers/BuscarController.php';
                 BuscarController::index();
                 break;
-                        
+            //Acceder a favoritos    
             case '/favoritos':
                 require_once 'controllers/FavoritoController.php';
                 FavoritoController::mostrarVista();
                 break;
-
+        
             case '/favoritos/toggle':
                 require_once 'controllers/FavoritoController.php'; 
                 FavoritoController::alternar();
                 break;
 
-
+            //Acceder a editar Perfil
             case '/editPerfil':
                 PerfilController::editar();
                 break;
 
-
+            // Cerrar Sesión
             case '/logout':
                 LoginController::logout();
                 break;
-
+            //Agregar al carrito 
             case '/carrito/agregar':
                 require_once 'controllers/CarritoController.php';
                 CarritoController::agregar();
@@ -123,48 +124,43 @@ switch ($method) {
                 require_once 'controllers/CarritoController.php';
                 CarritoController::borrarCarrito();
                 break;  
-
             
             // Gestiones 
-
             case '/gestion':
                 require_once 'views/gestion.php';
                 break;
-
-                
+            //Gestión de usuarios
             case '/gestionUsuarios':
                 require_once 'controllers/AdminController.php';
                 AdminController::gestionUsuarios();
                 break;
-
+            // Gestión de Productos
             case '/gestionProductos':
                 require_once 'controllers/AdminController.php';
                 AdminController::gestionProductos();
                 break;
-
+            //Gestión de añadir Producto
             case '/crearProducto':
                 require_once 'controllers/AdminController.php';
                 AdminController::mostrarFormularioProducto();
                 break;
-
-
+            //Gestión de eliminar Producto
             case '/eliminarProducto':
                 require_once 'controllers/AdminController.php';
                 AdminController::eliminarProducto();
                 break;
-
+            //Gestión de editar Producto
             case '/editarProducto':
-            require_once 'controllers/AdminController.php';
-            AdminController::mostrarEditarProducto();
-            break;
-
+                require_once 'controllers/AdminController.php';
+                AdminController::mostrarEditarProducto();
+                break;
+            //Gestión de eliminar Usuario
             case '/eliminarUsuario':
-            require_once 'controllers/AdminController.php';
-            AdminController::eliminarUsuario();
-            break;
+                require_once 'controllers/AdminController.php';
+                AdminController::eliminarUsuario();
+                break;
 
-            
-        
+            // Si no encuentra ningun resultado se va a la vista del error:404
             default:
                 require_once 'views/404.php';
                 break;
@@ -172,18 +168,19 @@ switch ($method) {
         }
         
     break;
-    
+
+    // Rutas que procesan formularios y envíos de datos
     case 'POST':
         switch ($request) {
-
+            //Accedemos al Login 
             case '/login':
                 LoginController::procesarLogin();
                 break;
-            
+            // Nos Registramos
             case '/registrarUsuario':
                 require_once 'controllers/RegistroController.php';  
                 break;
-
+            // Actualizamos nuestro Perfil
             case '/actualizarPerfil':
                     PerfilController::actualizar();
                     break;
@@ -193,15 +190,15 @@ switch ($method) {
                 require_once 'controllers/AdminController.php';
                 AdminController::guardarProducto();
                 break;
-
+            //Actualizamos los productos
             case '/actualizarProducto':
                 require_once 'controllers/AdminController.php';
                 AdminController::actualizarProducto();
                 break;
             
             default:
-            echo "Error, método no permitido";
-            break;
+                echo "Error, método no permitido";
+                break;
             
         }
         
